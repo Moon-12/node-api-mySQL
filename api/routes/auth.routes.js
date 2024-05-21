e6ccd43451d29037eaa107bfb478d3df314fafe9
Moon-12 <ashwijanayak@gmail.com>
2024-05-21 18:04:50 -0400

    auth controller and routes added

---
+++ api/routes/auth.routes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { verifySignUp } = require("../../middleware");
+const controller = require("../../controllers/auth.controller");
+const router = express.Router();
+
+router.post("/signup", [
+  verifySignUp.checkDuplicateUsernameOrEmail,
+  verifySignUp.checkRolesExisted,
+  controller.signup,
+]);
+
+router.get("/login", controller.login);
+// router.get("/:id", (req, res, next) => {
+//   const id = req.params.id;
+//   if (id === "special") {
+//     res.status(200).json({
+//       messgae: "you discovered special id",
+//       id: id,
+//     });
+//   } else {
+//     res.status(200).json({
+//       message: "you passed an id",
+//       id: id,
+//     });
+//   }
+// });
+
+module.exports = router;
